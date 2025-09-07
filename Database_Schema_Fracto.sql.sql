0172757c9f3cdea175cd41838d6f93118a3dc638
Naveena-Grace <raju.mosa888@gmail.com>
2025-09-07 23:17:57 +0530

    Add files via upload

---
+++ Database_Schema_Fracto.sql.sql
@@ -0,0 +1,20 @@
+--Creates a new database named Fractodatabase
+create database Fractodatabase
+
+use Fractodatabase
+
+--Retrieves all columns and rows from the 'Users' table
+select * from Users
+
+--Retrieves all columns and rows from the 'Specializations' table
+select * from Specializations
+
+--Retrieves all columns and rows from the 'Doctors' table
+select * from Doctors
+
+--Retrieves all columns and rows from the 'Ratings' table
+select * from Ratings
+
+--Retrieves all columns and rows from the 'Appointments' table
+select * from Appointments
+
